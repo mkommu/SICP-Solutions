383a73b91243897af0ccf7c1ab0f3b7b50ab0e96
Madhu Mohan Kommu <madhumohan.kommu@gmail.com>
2018-08-23 10:28:24 -0400

    Create CombineSumProduct_1.32.scala

---
+++ chapter1/CombineSumProduct_1.32.scala
@@ -0,0 +1,19 @@
+
+
+object CombineSumProduct111 {
+
+  def main(args: Array[String]): Unit = {
+
+    def mapReduce(f: Int => Int, combine: (Int, Int) => Int, zero: Int)(a: Int, b: Int): Int =
+      if( a > b) zero
+      else combine(f(a), mapReduce(f, combine, zero)(a + 1, b))
+
+    def product(f: Int => Int)(a: Int, b: Int): Int = mapReduce(f, (x,y) => x * y, 1)(a, b)
+
+    product(x => x * x)(3,4)
+
+    def fact(n: Int) = product(x => x)(1, n)
+    val factorial = fact(5)
+    println("---------------->"+factorial)
+  }
+}
